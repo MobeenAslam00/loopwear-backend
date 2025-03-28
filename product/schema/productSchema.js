debae805cf9674eeb435ab0763565135896b850f
ZAINY <134195524+ZainyShorts@users.noreply.github.com>
2025-03-29 04:35:03 +0500

    Update productSchema.js

--- product/schema/productSchema.js
+++ product/schema/productSchema.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema(
     category: { type: String, enum: ['Bridal', 'Party Wear', 'Non-Bridal'], required: true },
     productSize: { type: String, required: true },
     productListing: { type: String, required: true },
-    dateOfPurchase: { type: Date, required: true },
+    dateOfPurchase: { type: Date, required: false, default:"2025-03-29"},
     productDetails: { type: String, required: true },
     images: { type: [String], required: false }, 
   },
